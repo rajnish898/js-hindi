369a8f2734f75b2d53a268209ec4bd792052319c
Rajnish singh <rajnishsingh99503@gmail.com>
2025-07-26 05:02:46 +0000

    conversionoperation

---
+++ 01basics/03_conversionoperation.js
@@ -0,0 +1,27 @@
+let score = undefined;
+
+//console.log(typeof score);
+
+
+//console.log(typeof(score));
+
+let isNumbered = Number(score);
+
+//console.log(typeof isNumbered);
+//console.log(isNumbered);
+
+let number = 1
+let isloggedin = Boolean(number)
+
+console.log(isloggedin);
+console.log(number);
+
+let date = 33
+let tostring = String(date)
+
+console.log(tostring);
+console.log(typeof date);
+
+
+
+
