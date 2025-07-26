50a825551fd6efa8bd5c94f31bb765ac0946f0d1
Rajnish singh <rajnishsingh99503@gmail.com>
2025-07-26 04:06:59 +0000

    datatypes

---
+++ 01basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 14453
+let accountEmail = "rajnishsingh@123"
+var accountPassword = "12345";
+accountCity = "Jaipur"
+
+
+
+
+accountEmail ="hcA@13234.com"
+accountPassword = "233243"
+accountCity = "bangolore"
+let accountState;
+
+// accountId =2
+
+/*
+prefer not to use var b/c of issue in bloack scope and functional scope
+*/
+console.log(accountId)
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
---
+++ 01basics/02_dataTypes.js
@@ -0,0 +1,3 @@
+"use strict" //convert the code into new version (js)
+//alert(3+4) we use node.js not a browser
+console.log("rajnish singh")
