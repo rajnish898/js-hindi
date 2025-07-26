75285c83b13d8149cdee55a866bc5125028231b0
Rajnish singh <rajnishsingh99503@gmail.com>
2025-07-26 04:36:02 +0000

    dataTypes

--- 01basics/02_dataTypes.js
+++ 01basics/02_dataTypes.js
@@ -1,3 +1,18 @@
 "use strict" //convert the code into new version (js)
 //alert(3+4) we use node.js not a browser
 console.log("rajnish singh")
+
+let name = "rajnish singh"
+
+let age = 32
+let tre = true
+let backBachers;
+
+// null -> object
+//undefined -> undefined
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+console.log(backBachers);
