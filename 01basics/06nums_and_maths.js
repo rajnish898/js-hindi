9cd04acaede74cb9ac897865accfe1a26cf19ee5
Rajnish singh <rajnishsingh99503@gmail.com>
2025-08-27 10:31:20 +0000

    math and num

---
+++ 01basics/06nums_and_maths.js
@@ -0,0 +1,63 @@
+//+++++++++++++++++Numbers +++++++++++++++
+
+const score = 400
+console.log(score);
+
+
+const balance = new Number(100)
+console.log(balance);
+
+//convert nnum into string
+
+console.log(balance.toString().length);
+
+
+// how many no. you dispaly after your decimal no.
+
+console.log(balance.toFixed(2));
+
+const number = 123.956
+//it give the pricise value of a no.
+
+console.log(number.toPrecision(3));
+
+
+
+const numbers = 10000000
+console.log(numbers.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++ Maths ++++++++++++++
+
+
+// console.log(Math.SQRT1_2);
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.3));
+
+// console.log(Math.ceil(4.3));
+
+//floor aalso use as ceil'
+
+console.log(Math.min(9,3,6,2,6));
+
+
+console.log(Math.random());
+
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) +1 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
