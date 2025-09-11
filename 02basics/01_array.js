8698df20392cdadd340e910e25cd8d46be61af4f
Rajnish singh <rajnishsingh99503@gmail.com>
2025-09-11 01:57:27 +0000

    array

---
+++ 02basics/01_array.js
@@ -0,0 +1,44 @@
+//Array
+
+const myarr = [0, 1, 2, 3, 4, 5]
+   //or
+const  myarr2  =  new Array(1, 2, 3, 4 )
+
+
+const myHeroes = ["skatiman","naagraj"]
+
+console.log(myarr[1]);
+
+
+//Array Methods
+
+// myarr.push(6); //is to push elemnt
+// myarr.push(7); //is to push elemnt
+// myarr.pop(); // by default is remove the last value of an arrray
+
+
+ //  myarr.unshift(0) to add value starting of aaray
+// myarr.shift()
+
+
+// console.log(myarr.includes(9));
+// console.log(myarr.indexOf(4));
+
+const newarr = myarr.join() 
+//join is use eo bind my array and convert into string
+
+
+// console.log(myarr);
+// console.log(newarr);
+// console.log(typeof newarr);
+
+
+//slice ,splice
+
+console.log("A" , myarr);
+ const myn1 = myarr.slice(1,3)
+ console.log(myn1);
+
+ //slice me last wala include nsahi hota ha
+ //splice me first se last tak include hota he
+
