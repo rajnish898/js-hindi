552329c7cee50d6058156d88366f620b0d03f842
Rajnish singh <rajnishsingh99503@gmail.com>
2025-09-15 12:21:31 +0000

    scopes

---
+++ 03basics/02_scopes.js
@@ -0,0 +1,27 @@
+
+
+let a = 300
+
+if(true) {
+
+let a =10
+const b = 20
+console.log("Inner:",a);
+function addsnum() {
+    
+}
+
+
+
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+
+ console.log(a);
+// console.log(b);
+
+
