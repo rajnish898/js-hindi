d86fa0cf19ecd6a3f540b8a371248d646a31aea4
Rajnish singh <rajnishsingh99503@gmail.com>
2025-09-15 12:08:10 +0000

    function

---
+++ 01basics/04_comparsion.js
@@ -0,0 +1,65 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+
+// 
+
+
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+///b/c the comparsion operator >,< treat as  0 and basically it covert to 0 that why in thirsd
+//  console it gives to output 0
+
+// --> console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined <= 0);
+//b/c not work same as null opreator
+
+
+
+/// --> === its checks value strictly
+
+console.log("2" == 2)
+console.log("2" === 2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
