c51113907de32af417f517d4f560c602f0f1e721
Rajnish singh <rajnishsingh99503@gmail.com>
2025-09-17 11:37:12 +0000

    scopes

--- 03basics/02_scopes.js
+++ 03basics/02_scopes.js
@@ -6,22 +6,62 @@ if(true) {
 
 let a =10
 const b = 20
-console.log("Inner:",a);
-function addsnum() {
-    
-}
-
-
+// console.log("Inner:",a);
 
 }
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}
-
-
- console.log(a);
+//  console.log(a);
 // console.log(b);
+
+//child class parent class ke property ko  access kar sakta he
+// par parent class child ke class ke property nahi access kar sakta
+
+function one() {
+    const username = "Rajnish"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+
+     two()
+    
+}
+
+// one()
+
+if(true) {
+    const username = "rajnish"
+    if(username == "rajnish") {
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+
+// ****************** interseting *********************
+ console.log(addone(5));
+  
+function addone(num)  {
+    return num +1;
+
+}
+
+ addTwo(5) // you dont use before declaraton 
+
+ const addTwo = function(num) {
+    return num+2;
+ }
+
+
+
+
+
 
 
