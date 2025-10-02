7934f1dbe76f988c2461d9c3223368b9e8f980b7
Rajnish singh <rajnishsingh99503@gmail.com>
2025-10-02 10:51:12 +0000

    arrow

--- 03basics/03_arrow.js
+++ 03basics/03_arrow.js
@@ -1,13 +1,13 @@
-const user ={
-    username: "rajnish",
-    price: 999 ,
+// const user ={
+//     username: "rajnish",
+//     price: 999 ,
 
-    welcomemessage: function() {
-        console.log(`${this.username} , welcome to website`);
-        console.log(this);
+//     welcomemessage: function() {
+//         console.log(`${this.username} , welcome to website`);
+//         console.log(this);
         
-    }
-}
+//     }
+// }
 // user.welcomemessage()
 // user.username = "sam"
 // user.welcomemessage()
@@ -26,9 +26,35 @@ const user ={
 
 //  chai()
 
-const chai = () => {
+
+// function decalartion with arrow function
+
+// const chai = function() {
+//      let username = "rajnish"
+//    console.log(this.username);
+
+// }
+const chai = () =>  {
      let username = "rajnish"
    console.log(this);
-
 }
-chai()
+
+// chai()
+
+// arrow function
+// const addtwo = (num1 , num2) => {
+//     return num1 + num2; //explicit return 
+
+// }
+
+//implicit return
+// const addtwo = (num1 , num2) => (num1 + num2) ;
+
+const addtwo = (num1 , num2) => ({username: "rajnish"} )
+
+
+// console.log(addtwo(3,4));
+
+const myArray = [2,5,7,2,5]
+
+ 
