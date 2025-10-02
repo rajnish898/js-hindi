b1a3886704973d25aa6a845caa7376f6fcb076e4
Rajnish singh <rajnishsingh99503@gmail.com>
2025-10-02 09:01:07 +0000

    array

---
+++ 05_iteration/two.js
@@ -0,0 +1,28 @@
+//While
+
+let index = 0
+while(index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index+=2
+    
+    
+}
+
+let myarray = ["flash","batman","superman"]
+
+let i = 0
+while (i < myarray.length) {
+    // console.log(`Value is ${myarray[i]}`)
+    i++;
+}
+
+//do while
+
+let score =11
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+    
+    
+} while (score <= 10);
