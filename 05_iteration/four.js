1d50ceb680b1322ffe7c079dfd709ba22665f154
Rajnish singh <rajnishsingh99503@gmail.com>
2025-10-02 10:50:46 +0000

    loops

---
+++ 05_iteration/four.js
@@ -0,0 +1,28 @@
+const myobject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+}
+
+
+for (const key in myobject) {
+    // console.log(`${key} shortcut is for ${myobject[key]}`);
+    
+}
+
+const programming = ["js","rb","py", "java","cpp"]
+
+for (const key in programming ) {
+//    console.log(programming[key]);
+   
+}
+
+const map = new Map()
+map.set('IN', "India")
+map.set('FR', "Frsnce")
+map.set('USA', "United STate OF America")
+map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
