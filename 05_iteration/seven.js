f9e0cbdb451ead160c04346f3abdc5e77b5be27d
Rajnish singh <rajnishsingh99503@gmail.com>
2025-10-04 08:44:46 +0000

    seven

--- 05_iteration/seven.js
+++ 05_iteration/seven.js
@@ -1,5 +1,22 @@
 const myNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
- const newNums = myNumber.map( (num) => num +10)
- console.log(newNums);
+//  const newNums = myNumber.map( (num) => {
+//      return num +10
+//     })
+
+// myNumber.forEach( (num)  => {
+//    console.log(  num + 10);
+  
+// }) 
+
+
+//chaining of map
+
+// const newnums = myNumber
+// .map( (nums) => nums *10 ) -> the value of first map is passed to the second map and so on
+// .map( (nums) => nums +1 )
+// .filter( (nums) => nums >= 40 )
+
+// console.log(newnums);
+
  
