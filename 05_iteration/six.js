d3c17a490605674ecaa9d9a5cd434d052824a07c
Rajnish singh <rajnishsingh99503@gmail.com>
2025-10-04 06:53:00 +0000

    six

---
+++ 05_iteration/six.js
@@ -0,0 +1,52 @@
+// const coding = ["js","rb","py", "java","cpp"]
+
+// const values = coding.forEach( (item)  => {
+//     //   console.log(item);
+//       return item
+      
+// })
+
+// console.log(values); -> not retun any value
+
+
+const mynums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newnums =  mynums.filter( (num) => {
+//       return num>4
+// } )
+
+// const newnums = []
+
+// mynums.forEach( (num) => {
+//     if(num>4){
+//         newnums.push(num)
+//     }
+// } )
+
+
+// console.log(newnums);
+
+// const books =[ {},{},{}]
+
+// const userbooks = books.filter( (bk) =>bk.genre === "programming" )
+
+
+// const books = [
+//     { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+//     { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+//     { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+//     { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+//     { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+//     { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+//     { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+//     { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+//     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+//   ];
+
+//   const newbooks = books.filter( (bk) => {
+//      return bk.genre === "Science" && bk.publish >1994
+//     } )
+//   console.log(newbooks);
+
+// this tells about how data fetch from database where data stored in multiple objects to form an aaray
+  
